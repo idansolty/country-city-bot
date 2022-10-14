ef67cea7634604c740783a7474074a8634ded5e9
idansolty <idanso2001@gmail.com>
2022-10-14 18:58:39 +0300

    idan not kicks everyone out :(

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
   // countryCity("בדיקה בוט ארץ עיר");
 });
 
-client.on('message', async message => {
+client.on('message_create', async message => {
   if (message.body === '!ping') {
     message.reply('pong');
   }
@@ -52,6 +52,48 @@ client.on('message', async message => {
     const onlineList = JSON.stringify(online, null, 2)
 
     message.reply(`משחקים פעילים בבוט זה: \n ${onlineList.slice(3, onlineList.length - 1)}`);
+  } else if (message.body.startsWith('!setNextTime ')) {
+    const params = message.body.slice(13).split("\n");
+
+    const [groupname, time] = params;
+
+    setNextTimeInfo(groupname, new Date(time))
+  } else if (message.body.startsWith('!setNextQuestion ')) {
+    const params = message.body.slice(17).split("\n");
+
+    const [groupname, question] = params;
+
+    setNextMessageInfo(groupname, question)
+  } else if (message.body === "!info") {
+    message.reply(`פונקציות אפשריות - 
+    !countryCity {שם קבוצה}
+    !stopCountryCity {שם קבוצה}
+    !setNextTime {שם קבוצה} (\\n)
+          {time}
+    !setNextQuestion {שם קבוצה} (\\n)
+          {question}
+    !online`)
+  } else if (message.body === "!debug") {
+    const chats = await new Promise((resolve, reject) => {
+      setTimeout(reject, 30 * 1000)
+      client.getChats().then(resolve);
+    })
+
+    const chat = chats.find((chat) => chat.name === "ארץ עיר האחרון שורד - 2🧠❓");
+
+    const messages = await chat.fetchMessages({ limit: 500 });
+
+    const msgs = messages.slice(messages.findIndex((searchedMessages => searchedMessages.id.id === messages[305].id.id)) + 1)
+
+    const uniqMessages = msgs.filter((m) => m.type !== "revoked");
+
+    const allUsersInGroup = chat.participants.filter((user) => !user.isAdmin);
+
+    const usersThatDidNotAnswer = allUsersInGroup.filter((id) => !uniqMessages.find(m => (m.author || m.from) === id.id._serialized))
+
+    const mappedIds = usersThatDidNotAnswer.map(user => user.id._serialized);
+
+    console.log(msgs);
   }
 
 });
@@ -61,30 +103,33 @@ client.initialize();
 const countryCity = async (groupName, chat) => {
   const choosenChat = chat;
 
-  let randomTime = randomTimeTommorow()
+  let randomTime = randomTimeToday()
 
   console.log("started countryCity!");
   while (true) {
-    setNextTimeInfo(groupName, randomTime);
-
-    console.log(`waiting for time : ${randomTime.toLocaleDateString()} ${randomTime.toLocaleTimeString()}`)
-    if (await waitTillTime(randomTime, groupName)) {
-      return;
-    }
-
     const letter = randomHebrewLetter();
 
     const type = types[randomNumber(0, types.length)]
 
     const message = `${type} שמתחיל באות ${letter}`
 
-    const sentMessage = await choosenChat.sendMessage(message);
+    setNextTimeInfo(groupName, randomTime);
+    setNextMessageInfo(groupName, message);
+
+    console.log(`waiting for time : ${randomTime.toLocaleDateString()} ${randomTime.toLocaleTimeString()}`)
+    if (await waitTillStart(randomTime, groupName)) {
+      return;
+    }
+
+    const object = online.find(group => group.id === groupName);
+
+    const sentMessage = await choosenChat.sendMessage(object.nextMessage);
 
     await choosenChat.setMessagesAdminsOnly(false);
 
-    pushHistoryInfo(groupName, sentMessage);
+    pushHistoryInfo(groupName, object.nextMessage);
 
-    if (await waitTillTime(new Date(randomTime.getTime() + ONE_HOUR_MILISECONDS), groupName)) {
+    if (await waitTillEnd(new Date(new Date().getTime() + ONE_HOUR_MILISECONDS), groupName)) {
       return;
     };
 
@@ -95,7 +140,7 @@ const countryCity = async (groupName, chat) => {
 
     const relevantMessages = messages.slice(messages.findIndex((searchedMessages => searchedMessages.id.id === sentMessage.id.id)) + 1)
 
-    const uniqMessages = _.uniqBy(relevantMessages, "from");
+    const uniqMessages = relevantMessages.filter((m) => m.type !== "revoked");
 
     const allUsersInGroup = choosenChat.participants.filter((user) => !user.isAdmin);
 
@@ -109,10 +154,15 @@ const countryCity = async (groupName, chat) => {
 }
 
 const pushHistoryInfo = (groupName, action) => {
+  const parsedObject = {
+    time: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+    message: action
+  };
+
   const index = online.findIndex(group => group.id === groupName);
   online[index].history ?
-    online[index].history.push(action) :
-    online[index].history = [action]
+    online[index].history.push(parsedObject) :
+    online[index].history = [parsedObject]
 }
 
 const setNextTimeInfo = (groupName, action) => {
@@ -120,15 +170,46 @@ const setNextTimeInfo = (groupName, action) => {
   online[index].nextTime = action;
 }
 
-const waitTillTime = async (time, groupName) => {
-  const timeToWait = new Date(time).getTime() - new Date().getTime();
+const setNextMessageInfo = (groupName, message) => {
+  const index = online.findIndex(group => group.id === groupName);
+  online[index].nextMessage = message;
+}
+
+const waitTillStart = async (time, groupName) => {
+  const timeToWait = Math.max(1, new Date(time).getTime() - new Date().getTime());
 
   const iteration = parseInt((timeToWait / (ONE_HOUR_MILISECONDS)) * 100) + 1;
+
+  const object = online.find(group => group.id === groupName);
 
   for (let i in [...Array(iteration).keys()]) {
     await delay(timeToWait / (iteration))
 
-    if (!online.find(group => group.id === groupName)) {
+    if (!object) {
+      return true;
+    }
+
+    if (object.nextTime < new Date()) {
+      return;
+    }
+  }
+
+  if (object.nextTime > new Date()) {
+    return waitTillStart(time, groupName);
+  }
+}
+
+const waitTillEnd = async (time, groupName) => {
+  const timeToWait = Math.max(1, new Date(time).getTime() - new Date().getTime());
+
+  const iteration = parseInt((timeToWait / (ONE_HOUR_MILISECONDS)) * 100) + 1;
+
+  const object = online.find(group => group.id === groupName);
+
+  for (let i in [...Array(iteration).keys()]) {
+    await delay(timeToWait / (iteration))
+
+    if (!object) {
       return true;
     }
   }
@@ -179,3 +260,16 @@ const randomTimeTommorow = () => {
   return randomDate(start, end);
 }
 
+const randomTimeToday = () => {
+  const now = new Date();
+
+  // TOMMOROW BETWEEN 7 TO 23->
+  const start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18)
+  const end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23)
+
+  // IN THE NEXT 10 SECONDS ->
+  // const start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds() + 10)
+  // const end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds() + 20)
+
+  return randomDate(start, end);
+}
